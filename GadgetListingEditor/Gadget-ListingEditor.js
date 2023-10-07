3838e505a50a23b189e5bb50e7387c669ad0b398
Jon Robson <jdlrobson@gmail.com>
2023-10-07 09:42:20 -0700

    Link to wiki page

--- GadgetListingEditor/Gadget-ListingEditor.js
+++ GadgetListingEditor/Gadget-ListingEditor.js
@@ -1,6 +1,7 @@
 /******************************************************************
 Listing Editor v3.0.0alpha
 	Source code: https://github.com/jdlrobson/-Gadget-Workshop
+	Wiki: https://en.wikivoyage.org/wiki/MediaWiki:Gadget-ListingEditor2023Main.js
 	Original author:
 	- torty3
 	Additional contributors:

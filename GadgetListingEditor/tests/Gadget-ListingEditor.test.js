218c34ef75f4b493fff00bfae4b7dbdc6efb537d
Jon Robson <jdlrobson@gmail.com>
2023-10-09 13:55:21 -0700

    Add jest and basic test

---
+++ GadgetListingEditor/tests/Gadget-ListingEditor.test.js
@@ -0,0 +1,18 @@
+const listingEditor = require( '../Gadget-ListingEditor.js' );
+const TEMPLATE_TYPE = {
+	'Cosa_vedere': 'see',
+	'Cosa_fare': 'do',
+	'Acquisti': 'buy',
+	'Dove_mangiare': 'eat',
+	'Come_divertirsi': 'drink',
+	'Dove_alloggiare': 'sleep',
+	'Eventi_e_feste': 'listing',
+	'Come arrivare': 'listing',
+	'Come spostarsi': 'listing'
+};
+describe( 'Gadget-ListingEditor', () => {
+	it( 'exports', () => {
+		const le = listingEditor( [ 0 ], TEMPLATE_TYPE );
+		expect( le.MODE_ADD ).toBe( 'add' );
+	} );
+} );

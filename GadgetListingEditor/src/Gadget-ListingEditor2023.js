2d8b4403c18316ab0e79508d9efa06c8859750c5
Jon Robson <jdlrobson@gmail.com>
2024-06-09 14:13:50 -0700

    Support new heading markup

--- GadgetListingEditor/src/Gadget-ListingEditor2023.js
+++ GadgetListingEditor/src/Gadget-ListingEditor2023.js
@@ -179,21 +179,34 @@ $(function() {
 	 * div cause any CSS or UI conflicts.
 	 */
 	var wrapContent = function() {
+		var isNewMarkup = $( '.mw-heading').length > 0;
 		// No need to wrap with ?useparsoid=1&safemode=1
-		if ( $( '.mw-heading3, .mw-heading2' ).length ) {
+		if ( $( 'section .mw-heading3, section .mw-heading2' ).length ) {
 			return;
 		}
 		// MobileFrontend use-case
 		if ( $( '.mw-parser-output > h2.section-heading' ).length ) {
 			$( '.mw-parser-output > section' ).addClass( 'mw-h2section' );
 		} else {
-			$('#bodyContent h2').each(function(){
-				$(this).nextUntil("h1, h2").addBack().wrapAll('<div class="mw-h2section" />');
+			if ( isNewMarkup ) {
+				$('#bodyContent').find('.mw-heading2').each(function(){
+					$(this).nextUntil(".mw-heading").addBack().wrapAll('<div class="mw-h2section" />');
+				});
+			} else {
+				$('#bodyContent').find('h2').each(function(){
+					$(this).nextUntil("h1, h2").addBack().wrapAll('<div class="mw-h2section" />');
+				});
+			}
+		}
+		if ( isNewMarkup ) {
+			$('#bodyContent').find('.mw-heading3').each(function(){
+				$(this).nextUntil(".mw-heading").addBack().wrapAll('<div class="mw-h3section" />');
+			});
+		} else {
+			$('#bodyContent').find('h3').each(function(){
+				$(this).nextUntil("h1, h2, h3").addBack().wrapAll('<div class="mw-h3section" />');
 			});
 		}
-		$('#bodyContent h3').each(function(){
-			$(this).nextUntil("h1, h2, h3").addBack().wrapAll('<div class="mw-h3section" />');
-		});
 	};
 
 	var isLoaded = false;

90e6124b71b04ff59320c5f89ea593c4c86e4808
Jon Robson <jdlrobson@gmail.com>
2024-06-25 21:46:57 -0700

    3.3.0

--- GadgetListingEditor/dist/Gadget-ListingEditor2023.js
+++ GadgetListingEditor/dist/Gadget-ListingEditor2023.js
@@ -1,5 +1,5 @@
 /**
- * Listing Editor v3.1.0
+ * Listing Editor v3.3.0
  * @maintainer Jdlrobson
  * Please upstream any changes you make here to https://github.com/jdlrobson/Gadget-Workshop/tree/master/GadgetListingEditor
  * Raise issues at https://github.com/jdlrobson/Gadget-Workshop/issues
@@ -28,9 +28,172 @@
  *		- Figure out how to get this to upload properly
  */
  //<nowiki>
-window.__WIKIVOYAGE_LISTING_EDITOR_VERSION__ = '3.1.0'
+window.__WIKIVOYAGE_LISTING_EDITOR_VERSION__ = '3.3.0'
 
 'use strict';
+
+var GadgetListingEditor2023 = {};
+
+/**
+ * Wrap the h2/h3 heading tag and everything up to the next section
+ * (including sub-sections) in a div to make it easier to traverse the DOM.
+ * This change introduces the potential for code incompatibility should the
+ * div cause any CSS or UI conflicts.
+ */
+
+const wrapContent = function() {
+    var isNewMarkup = $( '.mw-heading').length > 0;
+    // No need to wrap with ?useparsoid=1&safemode=1
+    if ( $( 'section .mw-heading3, section .mw-heading2' ).length ) {
+        return;
+    }
+    // MobileFrontend use-case
+    if ( $( '.mw-parser-output > h2.section-heading' ).length ) {
+        $( '.mw-parser-output > section' ).addClass( 'mw-h2section' );
+    } else {
+        if ( isNewMarkup ) {
+            $('#bodyContent').find('.mw-heading2').each(function(){
+                $(this).nextUntil(".mw-heading1, .mw-heading2").addBack().wrapAll('<div class="mw-h2section" />');
+            });
+        } else {
+            $('#bodyContent').find('h2').each(function(){
+                $(this).nextUntil("h1, h2").addBack().wrapAll('<div class="mw-h2section" />');
+            });
+        }
+    }
+    if ( isNewMarkup ) {
+        $('#bodyContent').find('.mw-heading3').each(function(){
+            $(this).nextUntil(".mw-heading1,.mw-heading2,.mw-heading3").addBack().wrapAll('<div class="mw-h3section" />');
+        });
+    } else {
+        $('#bodyContent').find('h3').each(function(){
+            $(this).nextUntil("h1, h2, h3").addBack().wrapAll('<div class="mw-h3section" />');
+        });
+    }
+};
+
+/**
+ * Utility function for appending the "add listing" link text to a heading.
+ */
+const insertAddListingPlaceholder = function(parentHeading, addMsg = '' ) {
+    const $pheading =  $(parentHeading);
+    const $headline = $(parentHeading).find( '.mw-headline' );
+    const editSection = $headline.length ? $headline.next('.mw-editsection') : $pheading.next( '.mw-editsection');
+    editSection.append(`<span class="mw-editsection-bracket">[</span><a href="javascript:" class="listingeditor-add">${addMsg}</a><span class="mw-editsection-bracket">]</span>`);
+};
+
+const getHeading = ( sectionId ) => {
+    // do not search using "#id" for two reasons. one, the article might
+    // re-use the same heading elsewhere and thus have two of the same ID.
+    // two, unicode headings are escaped ("è" becomes ".C3.A8") and the dot
+    // is interpreted by JQuery to indicate a child pattern unless it is
+    // escaped
+    const $nodeWithId = $(`[id="${sectionId}"]`);
+    if ( $nodeWithId.is( 'h2' )  ) {
+        return $nodeWithId;
+    } else {
+        return $nodeWithId.closest( 'h2' );
+    }
+};
+
+const getSectionElement = ( $headingElement ) => {
+    if ( $headingElement.is( '.section-heading' ) ) {
+        return $headingElement.next( 'section.mw-h2section' );
+    } else {
+        return $headingElement.closest( 'div.mw-h2section, section' );
+    }
+};
+
+/**
+ * Place an "add listing" link at the top of each section heading next to
+ * the "edit" link in the section heading.
+ */
+const addListingButtons = function( SECTION_TO_TEMPLATE_TYPE, addMsg = '' ) {
+    for (let sectionId in SECTION_TO_TEMPLATE_TYPE) {
+        const topHeading = getHeading( sectionId );
+        if (topHeading.length) {
+            insertAddListingPlaceholder(topHeading, addMsg);
+            const parentHeading = getSectionElement( topHeading );
+            $('h3', parentHeading).each(function() {
+                insertAddListingPlaceholder(this, addMsg);
+            });
+        }
+    }
+};
+
+var contentTransform$1 = {
+    addListingButtons,
+    wrapContent,
+    insertAddListingPlaceholder
+};
+
+// map section heading ID to the listing template to use for that section
+var sectionToTemplateType$1 = function ( DB_NAME = 'enwikivoyage' ) {
+    switch ( DB_NAME ) {
+        case 'frwikivoyage':
+            return {
+                Aller: 'Aller',
+                Circuler: 'Circuler',
+                Voir: 'Voir',
+                Faire: 'Faire',
+                Acheter: 'Acheter',
+                Manger: 'Manger',
+                Communiquer: 'Listing',
+                'Boire_un_verre_.2F_Sortir': 'Sortir',
+                Sortir: 'Sortir',
+                Se_loger: 'Se loger',
+                'S.C3.A9curit.C3.A9': 'Listing',
+                'G.C3.A9rer_le_quotidien': 'Représentation diplomatique',
+                Villes: 'Ville',
+                Autres_destinations: 'Destination',
+                Aux_environs: 'Destination'
+            };
+        case 'itwikivoyage':
+            return {
+                'Cosa_vedere': 'see',
+                'Cosa_fare': 'do',
+                'Acquisti': 'buy',
+                'Dove_mangiare': 'eat',
+                'Come_divertirsi': 'drink',
+                'Dove_alloggiare': 'sleep',
+                'Eventi_e_feste': 'listing',
+                'Come arrivare': 'listing',
+                'Come spostarsi': 'listing'
+            };
+        default:
+            return {
+                'See': 'see',
+                'Do': 'do',
+                'Buy': 'buy',
+                'Eat': 'eat',
+                'Drink': 'drink',
+                'Sleep': 'sleep',
+                'Connect': 'listing',
+                'Wait': 'see',
+                'See_and_do': 'see',
+                'Eat_and_drink': 'eat',
+                'Get_in': 'go',
+                'Get_around': 'go',
+                'Anreise': 'station', // go
+                'Mobilität': 'public transport', // go
+                'Sehenswürdigkeiten': 'monument', // see
+                'Aktivitäten': 'sports', // do
+                'Einkaufen': 'shop', // buy
+                'Küche': 'restaurant', // eat
+                'Nachtleben': 'bar', // drink
+                // dummy line (es) // drink and night life
+                'Unterkunft': 'hotel', // sleep
+                'Lernen': 'education', // education
+                'Arbeiten': 'administration', // work
+                'Sicherheit': 'administration', // security
+                'Gesundheit': 'health', // health
+                'Praktische_Hinweise': 'office' // practicalities
+            };
+    }
+};
+
+const contentTransform = contentTransform$1;
+const sectionToTemplateType = sectionToTemplateType$1;
 
 $(function() {
 	const USE_LISTING_BETA = window.__USE_LISTING_EDITOR_BETA__;
@@ -49,70 +212,7 @@ $(function() {
 	// --------------------------------------------------------------------
 
 	var DB_NAME = mw.config.get( 'wgDBname' );
-	// map section heading ID to the listing template to use for that section
-	var SECTION_TO_TEMPLATE_TYPE = ( function () {
-		switch ( DB_NAME ) {
-			case 'frwikivoyage':
-				return {
-					Aller: 'Aller',
-					Circuler: 'Circuler',
-					Voir: 'Voir',
-					Faire: 'Faire',
-					Acheter: 'Acheter',
-					Manger: 'Manger',
-					Communiquer: 'Listing',
-					'Boire_un_verre_.2F_Sortir': 'Sortir',
-					Sortir: 'Sortir',
-					Se_loger: 'Se loger',
-					'S.C3.A9curit.C3.A9': 'Listing',
-					'G.C3.A9rer_le_quotidien': 'Représentation diplomatique',
-					Villes: 'Ville',
-					Autres_destinations: 'Destination',
-					Aux_environs: 'Destination'
-				};
-			case 'itwikivoyage':
-				return {
-					'Cosa_vedere': 'see',
-					'Cosa_fare': 'do',
-					'Acquisti': 'buy',
-					'Dove_mangiare': 'eat',
-					'Come_divertirsi': 'drink',
-					'Dove_alloggiare': 'sleep',
-					'Eventi_e_feste': 'listing',
-					'Come arrivare': 'listing',
-					'Come spostarsi': 'listing'
-				};
-			default:
-				return {
-					'See': 'see',
-					'Do': 'do',
-					'Buy': 'buy',
-					'Eat': 'eat',
-					'Drink': 'drink',
-					'Sleep': 'sleep',
-					'Connect': 'listing',
-					'Wait': 'see',
-					'See_and_do': 'see',
-					'Eat_and_drink': 'eat',
-					'Get_in': 'go',
-					'Get_around': 'go',
-					'Anreise': 'station', // go
-					'Mobilität': 'public transport', // go
-					'Sehenswürdigkeiten': 'monument', // see
-					'Aktivitäten': 'sports', // do
-					'Einkaufen': 'shop', // buy
-					'Küche': 'restaurant', // eat
-					'Nachtleben': 'bar', // drink
-					// dummy line (es) // drink and night life
-					'Unterkunft': 'hotel', // sleep
-					'Lernen': 'education', // education
-					'Arbeiten': 'administration', // work
-					'Sicherheit': 'administration', // security
-					'Gesundheit': 'health', // health
-					'Praktische_Hinweise': 'office' // practicalities
-				};
-		}
-	}() );
+	const SECTION_TO_TEMPLATE_TYPE = sectionToTemplateType( DB_NAME );
 	// selector that identifies the HTML elements into which the 'edit' link
 	// for each listing will be placed
 	var EDIT_LINK_CONTAINER_SELECTOR = 'span.listing-metadata-items';
@@ -132,7 +232,7 @@ $(function() {
 
 	// If any of these patterns are present on a page then no 'add listing'
 	// buttons will be added to the page
-	var DISALLOW_ADD_LISTING_IF_PRESENT = ( function () {
+	const DISALLOW_ADD_LISTING_IF_PRESENT = ( function () {
 		switch ( DB_NAME ) {
 			case 'frwikivoyage':
 				return [ '#R\u00E9gions', '#\u00EEles' ];
@@ -206,29 +306,7 @@ $(function() {
 		return true;
 	};
 
-	/**
-	 * Wrap the h2/h3 heading tag and everything up to the next section
-	 * (including sub-sections) in a div to make it easier to traverse the DOM.
-	 * This change introduces the potential for code incompatibility should the
-	 * div cause any CSS or UI conflicts.
-	 */
-	var wrapContent = function() {
-		// No need to wrap with ?useparsoid=1&safemode=1
-		if ( $( '.mw-heading3, .mw-heading2' ).length ) {
-			return;
-		}
-		// MobileFrontend use-case
-		if ( $( '.mw-parser-output > h2.section-heading' ).length ) {
-			$( '.mw-parser-output > section' ).addClass( 'mw-h2section' );
-		} else {
-			$('#bodyContent h2').each(function(){
-				$(this).nextUntil("h1, h2").addBack().wrapAll('<div class="mw-h2section" />');
-			});
-		}
-		$('#bodyContent h3').each(function(){
-			$(this).nextUntil("h1, h2, h3").addBack().wrapAll('<div class="mw-h3section" />');
-		});
-	};
+	const wrapContent = contentTransform.wrapContent;
 
 	var isLoaded = false;
 	function importForeignModule() {
@@ -288,65 +366,6 @@ $(function() {
 	};
 
 	/**
-	 * Utility function for appending the "add listing" link text to a heading.
-	 */
-	var insertAddListingPlaceholder = function(parentHeading) {
-		const $pheading =  $(parentHeading);
-		const $headline = $(parentHeading).find( '.mw-headline' );
-		const editSection = $headline.length ? $headline.next('.mw-editsection') : $pheading.next( '.mw-editsection');
-		const addMsg = USE_LISTING_BETA ? TRANSLATIONS.addBeta : TRANSLATIONS.add;
-		editSection.append(`<span class="mw-editsection-bracket">[</span><a href="javascript:" class="listingeditor-add">${addMsg}</a><span class="mw-editsection-bracket">]</span>`);
-	};
-
-	const getHeading = ( sectionId ) => {
-		// do not search using "#id" for two reasons. one, the article might
-		// re-use the same heading elsewhere and thus have two of the same ID.
-		// two, unicode headings are escaped ("è" becomes ".C3.A8") and the dot
-		// is interpreted by JQuery to indicate a child pattern unless it is
-		// escaped
-		const $nodeWithId = $(`[id="${sectionId}"]`);
-		if ( $nodeWithId.is( 'h2' )  ) {
-			return $nodeWithId;
-		} else {
-			return $nodeWithId.closest( 'h2' );
-		}
-	};
-
-	const getSectionElement = ( $headingElement ) => {
-		if ( $headingElement.is( '.section-heading' ) ) {
-			return $headingElement.next( 'section.mw-h2section' );
-		} else {
-			return $headingElement.closest( 'div.mw-h2section, section' );
-		}
-	};
-
-	/**
-	 * Place an "add listing" link at the top of each section heading next to
-	 * the "edit" link in the section heading.
-	 */
-	var addListingButtons = function() {
-		if ($(DISALLOW_ADD_LISTING_IF_PRESENT.join(',')).length > 0) {
-			return false;
-		}
-		for (var sectionId in SECTION_TO_TEMPLATE_TYPE) {
-			const topHeading = getHeading( sectionId );
-			if (topHeading.length) {
-				insertAddListingPlaceholder(topHeading);
-				var parentHeading = getSectionElement( topHeading );
-				$('h3', parentHeading).each(function() {
-					insertAddListingPlaceholder(this);
-				});
-			}
-		}
-		$('.listingeditor-add').on('click', function() {
-			var $this = $(this);
-			loadMain().then( function ( core ) {
-				core.initListingEditorDialog(core.MODE_ADD, $this);
-			} );
-		});
-	};
-
-	/**
 	 * Called on DOM ready, this method initializes the listing editor and
 	 * adds the "add/edit listing" links to sections and existing listings.
 	 */
@@ -355,8 +374,27 @@ $(function() {
 			return;
 		}
 		wrapContent();
-		mw.hook( 'wikipage.content' ).add( addListingButtons );
+		mw.hook( 'wikipage.content' ).add(
+			() => {
+
+				if ($(DISALLOW_ADD_LISTING_IF_PRESENT.join(',')).length > 0) {
+					return false;
+				}
+				contentTransform.addListingButtons(
+					SECTION_TO_TEMPLATE_TYPE,
+					USE_LISTING_BETA ? TRANSLATIONS.addBeta : TRANSLATIONS.add
+				);
+				$('.listingeditor-add').on('click', function() {
+					const $this = $(this);
+					loadMain().then( function ( core ) {
+						core.initListingEditorDialog(core.MODE_ADD, $this);
+					} );
+				});
+			}
+		);
 		addEditButtons();
 	};
 	initListingEditor();
 });
+
+module.exports = GadgetListingEditor2023;

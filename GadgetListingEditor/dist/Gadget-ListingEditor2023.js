b86a30f711d27e2f0add182aef765ea5dba0d0b5
Jon Robson <jdlrobson@gmail.com>
2025-07-29 09:02:07 -0700

    3.7.2

--- GadgetListingEditor/dist/Gadget-ListingEditor2023.js
+++ GadgetListingEditor/dist/Gadget-ListingEditor2023.js
@@ -1,5 +1,5 @@
 /**
- * Listing Editor v3.7.1
+ * Listing Editor v3.7.2
  * @maintainer Jdlrobson
  * Please upstream any changes you make here to https://github.com/jdlrobson/Gadget-Workshop/tree/master/GadgetListingEditor
  * Raise issues at https://github.com/jdlrobson/Gadget-Workshop/issues
@@ -28,7 +28,7 @@
  *		- Figure out how to get this to upload properly
  */
  //<nowiki>
-window.__WIKIVOYAGE_LISTING_EDITOR_VERSION__ = '3.7.1'
+window.__WIKIVOYAGE_LISTING_EDITOR_VERSION__ = '3.7.2'
 
 'use strict';
 

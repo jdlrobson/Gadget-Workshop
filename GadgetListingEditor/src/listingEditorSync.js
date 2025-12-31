7e99fe8ce6d40ac2832a0f89edf9f2f8af306f87
Jon Robson <jdlrobson@gmail.com>
2025-12-30 17:02:21 -0800

    Extract getSyncValues

--- GadgetListingEditor/src/listingEditorSync.js
+++ GadgetListingEditor/src/listingEditorSync.js
@@ -1,79 +1,8 @@
-const { iata } = require( './templates.js' );
-const prepareRadio = require( './prepareRadio.js' );
-const trimDecimal = require( './trimDecimal.js' );
-const { getConfig } = require( './Config.js' );
-const { translate } = require( './translate.js' );
 const ListingSyncRow = require( './components/ListingSyncRow.js' );
+const getSyncValues = require( './getSyncValues.js' );
 
 // @todo move to Vue component
 const ListingEditorSync = ( jsonObj, wikidataRecord ) => {
-    const SisterSite = require( './SisterSite.js' )();
-    const { wikidataClaim, wikidataWikipedia } = SisterSite;
-    const { WIKIDATA_CLAIMS } = getConfig();
-
-    const res = {};
-    for (let key in WIKIDATA_CLAIMS) {
-        res[key] = {};
-        res[key].value = wikidataClaim(jsonObj, wikidataRecord, WIKIDATA_CLAIMS[key].p);
-        res[key].guidObj = wikidataClaim(jsonObj, wikidataRecord,
-            WIKIDATA_CLAIMS[key].p, true);
-        if (key === 'iata') {
-            if( res[key].value ) {
-                res[key].value = iata.replace( '%s', res[key].value );
-            }
-        } else if (key === 'email') {
-            if( res[key].value ) {
-                res[key].value = res[key].value.replace('mailto:', '');
-            }
-        } else if (key === 'coords') {
-            if ( res[key].value ) {
-                res[key].value.latitude = trimDecimal(res[key].value.latitude, 6);
-                res[key].value.longitude = trimDecimal(res[key].value.longitude, 6);
-            }
-        }
-    }
-    const syncValues = [];
-    for (let key in res) {
-        const value = res[key].value;
-        const guidObj = res[key].guidObj;
-        if (key === 'coords' && value) {
-            const radio = prepareRadio(
-                WIKIDATA_CLAIMS[key],
-                [ value.latitude, value.longitude],
-                guidObj
-            );
-            if ( !radio.skip ) {
-                syncValues.push(
-                    radio
-                );
-            }
-        } else {
-            const radio = prepareRadio(
-                WIKIDATA_CLAIMS[key],
-                [ value ],
-                guidObj
-            );
-            if ( !radio.skip ) {
-                syncValues.push(
-                    radio
-                );
-            }
-        }
-    }
-    var wikipedia = wikidataWikipedia(jsonObj, wikidataRecord);
-    syncValues.push(
-        prepareRadio(
-            {
-                label: translate( "sharedWikipedia" ),
-                fields: ['wikipedia'],
-                doNotUpload: true,
-                'remotely_sync': true
-            },
-            [wikipedia],
-            $('#input-wikidata-value').val()
-        )
-    );
-
     return {
         name: 'ListingEditorSync',
         components: {
@@ -82,7 +11,7 @@ const ListingEditorSync = ( jsonObj, wikidataRecord ) => {
         props: {
             syncValues: {
                 type: Array,
-                default: syncValues
+                default: getSyncValues( jsonObj, wikidataRecord )
             }
         },
         template: `<form id="listing-editor-sync">

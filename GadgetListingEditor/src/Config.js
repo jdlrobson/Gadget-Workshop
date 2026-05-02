fb3d905c107252e46e317bccb9cbde5e59db85a8
Jon Robson <jdlrobson@gmail.com>
2026-05-02 10:42:21 -0700

    More type declarations

--- GadgetListingEditor/src/Config.js
+++ GadgetListingEditor/src/Config.js
@@ -1,3 +1,53 @@
+/**
+ * @typedef {Object} ListingWikidataClaimValue
+ * @property {string} p
+ * @property {string} label
+ * @property {string[]} fields
+ * @property {boolean} remotely_sync
+ */
+/**
+ * @typedef {Record<string, ListingWikidataClaimValue>} ListingWikidataClaims
+*/
+/**
+ * @typedef {Object} ListingTemplateParameterConfig
+ * @property {string} id
+ * @property {string|null} [hideDivIfEmpty]
+ * @property {boolean} [skipIfEmpty]
+ * @property {boolean} [newline]
+ */
+/**
+ * @typedef {Record<string, ListingTemplateParameterConfig>} ListingTemplateParametersConfig
+ */
+/**
+ * @typedef {Record<string, ListingTemplateParametersConfig>} ListingTemplateConfig
+ */
+/**
+ * @typedef {Object} ListingConfig
+ * @property {boolean} SHOW_LAST_EDITED_FIELD
+ * @property {string[]} SUPPORTED_SECTIONS
+ * @property {Record<string, string>} sectionType
+ * @property {string} iata
+ * @property {number} COORD_PRECISION
+ * @property {string} EDITOR_TAG
+ * @property {string} listingTypeRegExp
+ * @property {Object} SECTION_TO_TEMPLATE_TYPE
+ * @property {boolean} APPEND_FULL_STOP_TO_DESCRIPTION
+ * @property {boolean} REPLACE_NEW_LINE_CHARS
+ * @property {string[]} LISTING_TEMPLATES_OMIT
+ * @property {boolean} VALIDATE_CALLBACKS_EMAIL
+ * @property {boolean} SUBMIT_FORM_CALLBACKS_UPDATE_LAST_EDIT
+ * @property {boolean} ALLOW_UNRECOGNIZED_PARAMETERS_LOOKUP
+ * @property {string} LISTING_TYPE_PARAMETER
+ * @property {string} LISTING_CONTENT_PARAMETER
+ * @property {string} DEFAULT_LISTING_TEMPLATE
+ * @property {ListingTemplateParametersConfig} SLEEP_TEMPLATE_PARAMETERS
+ * @property {ListingTemplateParametersConfig} LISTING_TEMPLATE_PARAMETERS
+ * @property {string} WIKIDATAID
+ * @property {string[]} SPECIAL_CHARS
+ * @property {ListingTemplateConfig} LISTING_TEMPLATES
+ * @property {ListingWikidataClaims} WIKIDATA_CLAIMS
+ */
+
 /** @type {Partial<ListingConfig>} */
 let config = {};
 /**
--- GadgetListingEditor/src/types.js
+++ GadgetListingEditor/src/types.js
@@ -1,40 +1,24 @@
+
 /**
- * @typedef {Object} ListingTemplateParameterConfig
- * @property {string} id
- * @property {string|null} [hideDivIfEmpty]
- * @property {boolean} [skipIfEmpty]
- * @property {boolean} [newline]
+ * @callback TranslationFunction
+ * @param {string} name - The name of the user.
+ * @param {string[]} params
+ * @return {string}
  */
 /**
- * @typedef {Record<string, ListingTemplateParameterConfig>} ListingTemplateParametersConfig
+ * @typedef {Object} AbortableJQueryDeferred
+ * @property {Function} abort
  */
 /**
- * @typedef {Record<string, ListingTemplateParametersConfig>} ListingTemplateConfig
+ * @typedef {Object} CoordValue
+ * @property {string} latitude
+ * @property {string} longitude
  */
 /**
- * @typedef {Object} ListingConfig
- * @property {boolean} SHOW_LAST_EDITED_FIELD
- * @property {string[]} SUPPORTED_SECTIONS
- * @property {Record<string, string>} sectionType
- * @property {string} iata
- * @property {number} COORD_PRECISION
- * @property {string} listingTypeRegExp
- * @property {boolean} APPEND_FULL_STOP_TO_DESCRIPTION
- * @property {boolean} REPLACE_NEW_LINE_CHARS
- * @property {string[]} LISTING_TEMPLATES_OMIT
- * @property {boolean} VALIDATE_CALLBACKS_EMAIL
- * @property {boolean} SUBMIT_FORM_CALLBACKS_UPDATE_LAST_EDIT
- * @property {boolean} ALLOW_UNRECOGNIZED_PARAMETERS_LOOKUP
- * @property {string} LISTING_TYPE_PARAMETER
- * @property {string} LISTING_CONTENT_PARAMETER
- * @property {string} DEFAULT_LISTING_TEMPLATE
- * @property {ListingTemplateParametersConfig} SLEEP_TEMPLATE_PARAMETERS
- * @property {ListingTemplateParametersConfig} LISTING_TEMPLATE_PARAMETERS
- * @property {string} WIKIDATAID
- * @property {string[]} SPECIAL_CHARS
- * @property {ListingTemplateConfig} LISTING_TEMPLATES
- * @property {ListingWikidataClaims} WIKIDATA_CLAIMS
+ * @typedef {Object} WikidataClaimRecord
+ * @property {string|CoordValue} value
+ * @property {Object} guidObj
  */
 /**
- * @typedef {Record<string, Object>} ListingWikidataClaims
-*/
+ * @typedef {Object} RadioDefinition
+ */
